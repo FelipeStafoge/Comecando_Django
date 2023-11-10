7555dc7f2d60e1513c0d8c30ef13a050ebe9b5c2
FelipeStafoge <felipestafoge11@gmail.com>
2023-11-10 13:28:54 -0300

    Criação da url create e etc

--- contact/urls.py
+++ contact/urls.py
@@ -6,6 +6,7 @@ app_name = 'contact'
 
 urlpatterns = [
     path('contact/<int:contact_id>/', views.contact, name='contact'),
+    path('contact/create/', views.create, name='create'),
     path('search/', views.search, name='search'),
     path('', views.index, name='index'),
 ]
